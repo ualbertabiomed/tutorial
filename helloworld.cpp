04a1534954991fd9f078512838e2590798a07cb4
llim99 <55472571+llim99@users.noreply.github.com>
2019-09-18 00:02:05 -0600

    Create helloworld.cpp

---
+++ helloworld.cpp
@@ -0,0 +1,49 @@
+/*
+@brief: This program asks user for their name, and prints "Hello World, my name is <name>".
+		This program also prints out the Fibonacci sequence with 500 millisecond dealy, and putting all the output in output.txt.
+*/
+
+#include <iostream>
+#include <string>
+#include <windows.h>
+#include <fstream>
+using namespace std;
+
+
+	int main()
+{
+	int i = 0;
+	int fibonacci = 0;
+	string name;
+	ofstream myfile;
+
+	myfile.open("output.txt");
+
+	cout << "Please enter your name: ";
+	getline (cin, name);
+	cout << "Hello World, my name is " << name << ".\n";
+	
+	/* For output.txt*/
+	myfile<< "Please enter your name: ";
+	myfile << "Hello World, my name is " << name << ".\n";
+
+	while (i < 20)
+	{
+		fibonacci += 1;
+		cout << fibonacci << endl;
+		Sleep(500);
+
+	/*For output.txt*/
+		myfile << fibonacci;
+
+		i++;
+	}
+
+	cout << "Tast Complete!";
+
+	/*For output.txt*/
+	myfile << "Tast Complete!";
+
+	getchar();
+	return 0;
+}
