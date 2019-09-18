34de60f8b16ac90b4d55a6267536d47165a46750
dntrinh <dntrinh@ualberta.ca>
2019-09-18 00:16:58 -0600

    assignment done!

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 0859c73712267c7b47c717177e309b36dbae841c
 def helloworld():
 
     def greeting():
@@ -43,50 +39,4 @@ def helloworld():
     print("Task Complete!")
 
 
-<<<<<<< HEAD
-=======
-=======
-def helloworld():
-
-    def greeting():
-
-        print("What is your name?")
-        Name = input()
-        print("Hello World, my name is", Name)
-
-    greeting()
-
-    def fibonacci():
-
-        num_1 = 0
-        num_2 = 1
-
-        import time
-        time.sleep(1)
-        print(num_1)
-        import time
-        time.sleep(1)
-        print(num_2)
-        import time
-        time.sleep(1)
-
-        for i in range(1, 20):
-            new_num = num_1 + num_2
-            num_1 = num_2
-            num_2 = new_num
-            print(new_num)
-
-            import time
-            time.sleep(1)
-
-    fibonacci()
-
-    import time
-    time.sleep(1)
-
-    print("Task Complete!")
-
-
->>>>>>> 56cf3716c06986a76aca042c284b73a2eb574cda
->>>>>>> 0859c73712267c7b47c717177e309b36dbae841c
 helloworld()
